4682a555363590b85b7999211bc3647147879eac
Koyo Nakakita <nakakita54@gmail.com>
2017-07-28 16:10:36 +0000

    ログイン機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -10,7 +12,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
+  
   def create
     @post = Post.new(content: params[:content])
     if @post.save
@@ -28,21 +30,19 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-
     if @post.save
       flash[:notice] = "投稿を編集しました"
       redirect_to("/posts/index")
     else
       render("posts/edit")
-    end  
+    end
   end
-
+  
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
-
-
+  
 end
