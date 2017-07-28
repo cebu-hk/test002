ffc1fb824151697dfd6793c43796515d08de63c4
Koyo Nakakita <nakakita54@gmail.com>
2017-07-28 04:15:23 +0000

    lesson Ⅶまで反映（10/15）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  get "signup" => "users#new"
+
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
